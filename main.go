73f76b7d712fa1609a50d97a97f8e713eab70cbf
jou_2015MAC <jou66jou@gmail.com>
2019-05-16 22:38:24 +0800

    update

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"gopkgSpider/models"
 	"gopkgSpider/routers"
 	"log"
 	"net/http"
@@ -12,7 +13,7 @@ func main() {
 	fmt.Println("Main: url set-" + url)
 
 	// run spider gopkgs and insert to mysql
-	// md.SpiderGopkgs(url)
+	models.SpiderGopkgs(url)
 
 	// starts an api service with a given routers.
 	log.Fatal(http.ListenAndServe(":8081", routers.Routers()))
